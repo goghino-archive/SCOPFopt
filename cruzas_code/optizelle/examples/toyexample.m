aaa90cd7d48ea6244f63f601a997dc5002eb7eda
cruzas <cruzas@users.noreply.github.com>
2017-11-23 17:15:34 +0100

    toyexample: further settings options added
    Settings options added (settings start on line 283).

--- cruzas_code/optizelle/examples/toyexample.m
+++ cruzas_code/optizelle/examples/toyexample.m
@@ -27,7 +27,7 @@ end
 
 % Define a simple objective.
 % f(x) = sin(x(1)) + sin(x(2))
-function self = MyObj()
+function self = MyObj(myauxdata)
    
     % Evaluation 
     self.eval = @(x) sin(x(1)) + sin(x(2));
@@ -37,41 +37,44 @@ function self = MyObj()
                       cos(x(2))];
 
     % Hessian-vector product
-    self.hessvec = @(x,dx) MyHessvec(x,dx);
+    self.hessvec = @(x,dx) MyHessvec(x,dx, myauxdata);
                                 
     %% Helper functions
-   function hessvec = MyHessvec(x, dx)
+   function hessvec = MyHessvec(x, dx, myauxdata)
       functionIdentifier = '[MyObj] MyHessvec';
       
+      % Define Hessian matrix.
       H = [-sin(x(1)),        0; 
                     0, -sin(x(2))];
                  
       hessvec = H * dx;
-                                
-      %% Test for Inf, -Inf, and NaN in dx.
-      if find(dx == Inf)
-         fprintf('%s: Inf found in dx\n', functionIdentifier);
-      end
-      
-      if find(dx == -Inf)
-         fprintf('%s: -Inf found in dx\n', functionIdentifier);
-      end
-      
-      if find(isnan(dx))
-         fprintf('%s: NaN found in dx\n', functionIdentifier);
-      end
-      
-      %% Test for Inf, -Inf, and NaN in jvec.
-      if find(hessvec == Inf, 1)
-         fprintf('%s: Inf found in hessvec\n', functionIdentifier);
-      end
-      
-      if find(hessvec == -Inf, 1)
-         fprintf('%s: -Inf found in hessvec\n', functionIdentifier);
-      end
-      
-      if find(isnan(hessvec), 1)
-         fprintf('%s: NaN found in hessvec\n', functionIdentifier);
+                          
+      if myauxdata.verbose
+         %% Test for Inf, -Inf, and NaN in dx.
+         if find(dx == Inf)
+            fprintf('%s: Inf found in dx\n', functionIdentifier);
+         end
+         
+         if find(dx == -Inf)
+            fprintf('%s: -Inf found in dx\n', functionIdentifier);
+         end
+         
+         if find(isnan(dx))
+            fprintf('%s: NaN found in dx\n', functionIdentifier);
+         end
+         
+         %% Test for Inf, -Inf, and NaN in jvec.
+         if find(hessvec == Inf, 1)
+            fprintf('%s: Inf found in hessvec\n', functionIdentifier);
+         end
+         
+         if find(hessvec == -Inf, 1)
+            fprintf('%s: -Inf found in hessvec\n', functionIdentifier);
+         end
+         
+         if find(isnan(hessvec), 1)
+            fprintf('%s: NaN found in hessvec\n', functionIdentifier);
+         end
       end
    end
 end
@@ -96,7 +99,7 @@ function self = MyEq(myauxdata)
     % xhat=(g''(x)dx)*dy (Partial derivatives of Jacobian * dx)
     self.pps = @(x,dx,dy) MyHessvec(x, dx, dy, myauxdata);
                          
-    % Helper functions.
+    %% Helper functions.
    function jvec = MyJacobvec(x, d, myauxdata)
       functionIdentifier = '[MyEq] MyJacobvec';
       
@@ -113,36 +116,84 @@ function self = MyEq(myauxdata)
          dType = 'dy';
       end
       
-      %% Test for Inf, -Inf, and NaN in d.
-      if find(d == Inf)
-         fprintf('%s: Inf found in %s\n', functionIdentifier, dType);
-      end
-      
-      if find(d == -Inf)
-         fprintf('%s: -Inf found in %s\n', functionIdentifier, dType);
-      end
-      
-      if find(isnan(d))
-         fprintf('%s: NaN found in %s\n', functionIdentifier, dType);
-      end
-      
-      %% Test for Inf, -Inf, and NaN in jvec.
-      if find(jvec == Inf, 1)
-         fprintf('%s: Inf found in jvec\n', functionIdentifier)
-      end
-      
-      if find(jvec == -Inf, 1)
-         fprintf('%s: -Inf found in jvec\n', functionIdentifier)
-      end
-      
-      if find(isnan(jvec), 1)
-         fprintf('%s: NaN found in jvec\n', functionIdentifier)
+      if myauxdata.verbose
+         %% Test for Inf, -Inf, and NaN in d.
+         if find(d == Inf)
+            fprintf('%s: Inf found in %s\n', functionIdentifier, dType);
+         end
+         
+         if find(d == -Inf)
+            fprintf('%s: -Inf found in %s\n', functionIdentifier, dType);
+         end
+         
+         if find(isnan(d))
+            fprintf('%s: NaN found in %s\n', functionIdentifier, dType);
+         end
+         
+         %% Test for Inf, -Inf, and NaN in jvec.
+         if find(jvec == Inf, 1)
+            fprintf('%s: Inf found in jvec\n', functionIdentifier)
+         end
+         
+         if find(jvec == -Inf, 1)
+            fprintf('%s: -Inf found in jvec\n', functionIdentifier)
+         end
+         
+         if find(isnan(jvec), 1)
+            fprintf('%s: NaN found in jvec\n', functionIdentifier)
+         end
       end
    end
 
    function hessvec = MyHessvec(x, dx, dy, myauxdata)
-      hessvec = ([2, 0; 
-                  0, 2] * dx)' * dy; 
+      functionIdentifier = '[MyEq] MyHessvec';
+      
+      % Define Hessian matrix.
+      H = [2, 0; 
+           0, 2];
+        
+      hessvec = (H * dx)' * dy; 
+      
+      if myauxdata.verbose
+         %% Test for Inf, -Inf, and NaN in dx.
+         if find(dx == Inf, 1)
+            fprintf('%s: Inf found in dx\n', functionIdentifier)
+         end
+         
+         if find(dx == -Inf, 1)
+            fprintf('%s: -Inf found in dx\n', functionIdentifier)
+         end
+         
+         if find(isnan(dx), 1)
+            fprintf('%s: NaN found in dx\n', functionIdentifier)
+         end
+         
+         %% Test for Inf, -Inf, and NaN in dy.
+         if find(dy == Inf, 1)
+            fprintf('%s: Inf found in dy\n', functionIdentifier)
+         end
+         
+         if find(dy == -Inf, 1)
+            fprintf('%s: -Inf found in dy\n', functionIdentifier)
+         end
+         
+         if find(isnan(dy), 1)
+            fprintf('%s: NaN found in dy\n', functionIdentifier)
+         end
+         
+         %% Test for Inf, -Inf, and NaN in hessvec.
+         if find(hessvec == Inf, 1)
+            fprintf('%s: Inf found in hessvec\n', functionIdentifier)
+         end
+         
+         if find(hessvec == -Inf, 1)
+            fprintf('%s: -Inf found in hessvec\n', functionIdentifier)
+         end
+         
+         if find(isnan(hessvec), 1)
+            fprintf('%s: NaN found in hessvec\n', functionIdentifier)
+         end
+      end
    end
 end
 
@@ -164,22 +215,62 @@ function self = MyIneq(myauxdata)
                       myauxdata.xmax - x];
 
     % z=h'(x)dx
-    self.p = @(x,dx) [  -sin(x(1)),          0;
-                                 0, -sin(x(2));
-                                 sparse(eye(length(x)));
-                                -sparse(eye(length(x)))] * dx;
+    self.p = @(x,dx) MyJacobvec(x, dx, myauxdata);
 
     % xhat=h'(x)*dz
-    self.ps = @(x,dz) [-sin(x(1)),          0;
-                                0, -sin(x(2));
-                                0,          0;
-                                0,          0;
-                                0,          0;
-                                0,          0]' * dz;
+    self.ps = @(x,dz) MyJacobvec(x, dz, myauxdata);
 
     % xhat=(h''(x)dx)*dz
     % Since all constraints are affine, we have h''(x) = 0.
     self.pps = @(x,dx,dz) sparse(length(x), length(x)); 
+    
+    %% Helper functions.
+    function jvec = MyJacobvec(x, d, myauxdata)
+      functionIdentifier = '[MyIneq] MyJacobvec';
+      
+      % Define Jacobian matrix.
+      J = [  -sin(x(1)),          0;
+                      0, -sin(x(2));
+             sparse(eye(length(x)));
+            -sparse(eye(length(x)))];
+      
+      dType = 0;
+      if (size(d, 1) == size(x, 1))  % case: d == dx
+         dType = 'dx';
+         jvec = J * d;
+      elseif (size(d, 1) == myauxdata.NINEQ) % case: d == dz
+         jvec = J' * d;
+         dType = 'd';
+      end
+      
+      if myauxdata.verbose
+         %% Test for Inf, -Inf, and NaN in d.
+         if find(d == Inf)
+            fprintf('%s: Inf found in %s\n', functionIdentifier, dType);
+         end
+         
+         if find(d == -Inf)
+            fprintf('%s: -Inf found in %s\n', functionIdentifier, dType);
+         end
+         
+         if find(isnan(d))
+            fprintf('%s: NaN found in %s\n', functionIdentifier, dType);
+         end
+         
+         %% Test for Inf, -Inf, and NaN in jvec.
+         if find(jvec == Inf, 1)
+            fprintf('%s: Inf found in jvec\n', functionIdentifier)
+         end
+         
+         if find(jvec == -Inf, 1)
+            fprintf('%s: -Inf found in jvec\n', functionIdentifier)
+         end
+         
+         if find(isnan(jvec), 1)
+            fprintf('%s: NaN found in jvec\n', functionIdentifier)
+         end
+      end
+   end
 end
 
 % Actually runs the program
@@ -189,11 +280,11 @@ function main()
     global Optizelle;
     setupOptizelle();
     
-    % Settings.
-    % Toggle on/off for xmin = -inf(size(x0)); xmax = inf(size(x0));
+    % Settings...
     infiniteBounds = 0;
     bigButNotInfiniteBounds = 0;
-    
+    verbose = 1;
+
     NEQ = 2;   % Number of equality constraints.
     NINEQ = 6; % Number of inequality constraints.
 
@@ -220,6 +311,9 @@ function main()
     
     myauxdata.xmin = xmin;
     myauxdata.xmax = xmax;
+    myauxdata.NEQ = NEQ;
+    myauxdata.NINEQ = NINEQ;
+    myauxdata.verbose = verbose; % print out all information for testing
     
     % Create an optimization state
     state = Optizelle.Constrained.State.t( ...
@@ -227,7 +321,7 @@ function main()
 
     % Create a bundle of functions
     fns = Optizelle.Constrained.Functions.t;
-    fns.f = MyObj();
+    fns.f = MyObj(myauxdata);
     fns.g = MyEq(myauxdata);
     fns.h = MyIneq(myauxdata);
 
