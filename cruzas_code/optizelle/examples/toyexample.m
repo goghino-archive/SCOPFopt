aa573f6e4b93c89daf1acca187342522363200e0
cruzas <cruzas@users.noreply.github.com>
2017-11-23 16:49:14 +0100

    toyexample: adding helper functions
    In order to more closely resemble the code structure we have in original problem.

--- cruzas_code/optizelle/examples/toyexample.m
+++ cruzas_code/optizelle/examples/toyexample.m
@@ -28,7 +28,7 @@ end
 % Define a simple objective.
 % f(x) = sin(x(1)) + sin(x(2))
 function self = MyObj()
-
+   
     % Evaluation 
     self.eval = @(x) sin(x(1)) + sin(x(2));
 
@@ -37,8 +37,43 @@ function self = MyObj()
                       cos(x(2))];
 
     % Hessian-vector product
-    self.hessvec = @(x,dx) [-sin(x(1)),        0; 
-                                   0, -sin(x(2))] * dx;
+    self.hessvec = @(x,dx) MyHessvec(x,dx);
+                                
+    %% Helper functions
+   function hessvec = MyHessvec(x, dx)
+      functionIdentifier = '[MyObj] MyHessvec';
+      
+      H = [-sin(x(1)),        0; 
+                    0, -sin(x(2))];
+                 
+      hessvec = H * dx;
+                                
+      %% Test for Inf, -Inf, and NaN in dx.
+      if find(d == Inf)
+         fprintf('%s: Inf found in dx', functionIdentifier);
+      end
+      
+      if find(d == -Inf)
+         fprintf('%s: -Inf found in dx', functionIdentifier);
+      end
+      
+      if find(isnan(d))
+         fprintf('%s: NaN found in dx', functionIdentifier);
+      end
+      
+      %% Test for Inf, -Inf, and NaN in jvec.
+      if find(hessvec == Inf, 1)
+         fprintf('%s: Inf found in hessvec', functionIdentifier);
+      end
+      
+      if find(hessvec == -Inf, 1)
+         fprintf('%s: -Inf found in hessvec', functionIdentifier);
+      end
+      
+      if find(isnan(hessvec), 1)
+         fprintf('%s: NaN found in hessvec', functionIdentifier);
+      end
+   end
 end
 
 % Define a simple equality
@@ -46,23 +81,69 @@ end
 % g(x) = [x(1)^2 = 1;
 %         x(2)^2 = 1]         
 % Optizelle  requires g(x) = 0, so we transform g(x) accordingly.
-function self = MyEq()
+function self = MyEq(myauxdata)
 
     % y=g(x) 
     self.eval = @(x) [x(1)^2 - 1;
                       x(2)^2 - 1]; 
 
     % y=g'(x)dx (Jacobian * dx)
-   self.p = @(x,dx) [2*x(1),    0;
-                        0, 2*x(2)] * dx;
-    
+    self.p = @(x,dx) MyJacobvec(x, dx, myauxdata);
+
     % xhat=g'(x)*dy (Jacobian' * dy)
-    self.ps = @(x,dy) [2*x(1),    0;
-                        0,   2*x(2)]' * dy;
+    self.ps = @(x, dy) MyJacobvec(x, dy, myauxdata);
 
     % xhat=(g''(x)dx)*dy (Partial derivatives of Jacobian * dx)
-    self.pps = @(x,dx,dy) ([2, 0; 
-                            0, 2] * dx)' * dy; 
+    self.pps = @(x,dx,dy) MyHessvec(x, dx, dy, myauxdata);
+                         
+    % Helper functions.
+   function jacobvec = MyJacobvec(x, d, myauxdata)
+      functionIdentifier = '[MyEq] MyJacobvec';
+      
+      % Define Jacobian matrix.
+      J = [2*x(1),    0;
+                0, 2*x(2)];
+      
+      dType = 0;
+      if (size(d, 1) == size(x, 1))  % case: d == dx
+         dType = 'dx';
+         jacobvec = J * d;
+      elseif (size(d, 1) == myauxdata.NEQ) % case: d == dy
+         jacobvec = J' * d;
+         dType = 'dy';
+      end
+      
+      %% Test for Inf, -Inf, and NaN in d.
+      if find(d == Inf)
+         fprintf('%s: Inf found in %s\n', functionIdentifier, dType);
+      end
+      
+      if find(d == -Inf)
+         fprintf('%s: -Inf found in %s\n', functionIdentifier, dType);
+      end
+      
+      if find(isnan(d))
+         fprintf('%s: NaN found in %s\n', functionIdentifier, dType);
+      end
+      
+      %% Test for Inf, -Inf, and NaN in jvec.
+      if find(jvec == Inf, 1)
+         fprintf('%s: Inf found in jvec', functionIdentifier)
+      end
+      
+      if find(jvec == -Inf, 1)
+         fprintf('%s: -Inf found in jvec', functionIdentifier)
+      end
+      
+      if find(isnan(jvec), 1)
+         fprintf('%s: NaN found in jvec', functionIdentifier)
+      end
+   end
+
+   function hessvec = MyHessvec(x, dx, dy, myauxdata)
+      hessvec = ([2, 0; 
+                  0, 2] * dx)' * dy; 
+   end
 end
 
 % Define inequalities, and bounds on x.
@@ -147,14 +228,16 @@ function main()
     % Create a bundle of functions
     fns = Optizelle.Constrained.Functions.t;
     fns.f = MyObj();
-    fns.g = MyEq();
+    fns.g = MyEq(myauxdata);
     fns.h = MyIneq(myauxdata);
 
     % Solve the optimization problem
+    tic;
     state = Optizelle.Constrained.Algorithms.getMin( ...
         Optizelle.Rm,Optizelle.Rm,Optizelle.Rm,Optizelle.Messaging.stdout, ...
         fns,state);
-
+    myTime = toc
+     
     % Print out the reason for convergence
     fprintf('The algorithm converged due to: %s\n', ...
         Optizelle.OptimizationStop.to_string(state.opt_stop));
